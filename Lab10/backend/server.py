5a42171b244a8b18e9bf9d4d05d5791cc350578f
smw989 <145391441+smw989@users.noreply.github.com>
2024-03-27 18:39:30 -0600

    B

--- Lab10/backend/server.py
+++ Lab10/backend/server.py
@@ -1,8 +1,10 @@
+from flask_cors import CORS
 from flask import Flask, request, jsonify, send_from_directory
 import json
 import os
 
 app = Flask(__name__)
+CORS (app)
 
 def load_products():
     with open('products.json', 'r') as f:
